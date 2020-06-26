38e6dc97ae8ef2e311e01fac99cc32a3b1bf2978
Aliff <aliff.macapinlac@gmail.com>
2020-06-25 17:50:08 -0700

    init

---
+++ assets/js/homepage.js
@@ -0,0 +1,24 @@
+function getUserRepos(user) {
+    console.log("Get user repos");
+    fetch(`https://api.github.com/users/${user}/repos`)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            let main = document.querySelector('main');
+            console.log()
+            for (let i = 0; i < data.length; i++) {
+                let div = document.createElement('div')
+                div.textContent = data[i].name
+                console.log(data[i].name)
+                main.appendChild(div)
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+getUserRepos('aliffbm');
+
